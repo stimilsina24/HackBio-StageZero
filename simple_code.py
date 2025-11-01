62ceeab5be6c2598f4ed7ce9c5a99b659175ca87
Santosh Timilsina <stimilsina12@gmail.com>
2025-11-01 18:28:36 -0500

    updated code

--- simple_code.py
+++ simple_code.py
@@ -4,11 +4,11 @@ import pandas as pd
 #Creating dictionary with Column names (key) and Adding Member information next to each Column name (Values)
 data = {
   "Name": ["Santosh","Svetlana","Debangana","Hana E", "Gideon Danso", "Mosunmola Temitope Christianah", "Jegede Judah Olayemi", "Adewumi Esther Adeola"],
-  "Slack Username": ["Svetlana Sokolova","Santosh T","debangana","Hana E", "Joegidi4real", "Hana E", "Jegede Judah", "adewumiesther"],
-  "Country" :["United Kingdom","United States of America","United Kingdom","Canada", "Nigeria", "Nigeria", "Nigeria"],
-  "Hobby":["Playing Piano","Tennis","Badminton","Hiking", "Listening to music", "Cooking", "Playing Soccer", "Cooking"],
-  "Affiliation":["Queens University Belfast","University of Texas at San Antonio","Queens University Belfast (Alumna)","Boise State University", "University of Cape Coast", "Obafemi Awolowo University", "Obafemi Awolowo University", "Obafemi Awolowo University"],
-  "Favourite Gene":["TP53:TCCTACAGTACTCCCCTGCCCTCAACATGTG","IFNB1: ATTCTAACTGCAACCTTTCGAAGCCTTTGCTCTGGCAC","BRCA1: ATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAA","glnD: AGGAAACGCAGATAGCGGGGCCTTGCGGAGAGAG", "BRCA2: GACCACTCTAAGCTTTTGTAAGATCGGCTCGC", "TNNT2: AGTCCCCGCTGAGACTGAGCAGACGCCTCCAG", "POSTN: GCCAGTTCTCTTCGGGGACTAACTGCAACGGAGAGA", "TP53:TCCTACAGTACTCCCCTGCCCTCAACATGTG"]
+  "Slack Username": ["Santosh T","Svetlana Sokolova","debangana","Hana E", "Joegidi4real", "Hana E", "Jegede Judah", "adewumiesther"],
+  "Country" :["United States of America","United Kingdom","United Kingdom","Canada", "Nigeria", "Nigeria", "Nigeria"],
+  "Hobby":["Tennis","Playing Piano","Badminton","Hiking", "Listening to music", "Cooking", "Playing Soccer", "Cooking"],
+  "Affiliation":["University of Texas at San Antonio","Queens University Belfast","Queens University Belfast (Alumna)","Boise State University", "University of Cape Coast", "Obafemi Awolowo University", "Obafemi Awolowo University", "Obafemi Awolowo University"],
+  "Favourite Gene":["IFNB1: ATTCTAACTGCAACCTTTCGAAGCCTTTGCTCTGGCAC","TP53:TCCTACAGTACTCCCCTGCCCTCAACATGTG","BRCA1: ATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAA","glnD: AGGAAACGCAGATAGCGGGGCCTTGCGGAGAGAG", "BRCA2: GACCACTCTAAGCTTTTGTAAGATCGGCTCGC", "TNNT2: AGTCCCCGCTGAGACTGAGCAGACGCCTCCAG", "POSTN: GCCAGTTCTCTTCGGGGACTAACTGCAACGGAGAGA", "TP53:TCCTACAGTACTCCCCTGCCCTCAACATGTG"]
 }
 
 #Converting the dictionary to Dataframe with custom row names for better clarity 
